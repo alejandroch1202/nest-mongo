57c9ec17ddf8078c329e283e630dd8e63e24a1fb
alejandroch1202 <alejandrochavez1202@gmail.com>
2024-07-02 18:52:17 -0400

    Adding CRUD for pokemons

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,23 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    MongooseModule.forRoot(
+      'mongodb://root:example@localhost:27017/?directConnection=true',
+    ),
+
+    PokemonModule,
+
+    CommonModule,
+  ],
 })
 export class AppModule {}
